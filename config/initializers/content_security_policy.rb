12f558ebf0eaeaf8a82cf6b9b4e6a0efcc4a6317
ziraq young <ziraqyoung@outlook.com>
2021-06-20 09:16:18 +0300

    Setup tailwindcss jit

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,6 +4,9 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
+Rails.application.config.content_security_policy do |policy|
+  policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035' if Rails.env.development?
+end
 # Rails.application.config.content_security_policy do |policy|
 #   policy.default_src :self, :https
 #   policy.font_src    :self, :https, :data
